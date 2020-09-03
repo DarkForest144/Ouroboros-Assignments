b6a8f47c1aad2a093bbd832b83d5bea520de02ca
Abhishek <Abhishekkokategunzpro@gmail.com>
2020-09-03 20:12:43 +0530

    first commit

---
+++ Assignment-1.py
@@ -0,0 +1,19 @@
+# Question-  https://www.hackerrank.com/challenges/arrays-ds/problem
+
+def Rev(lst): 
+    return [ele for ele in reversed(lst)] 
+
+
+
+n = int(input('Enter size of array: '))
+lst = []
+
+for i in range(0, n): 
+    ele = int(input()) 
+  
+    lst.append(ele) # adding the element 
+      
+print(lst) 
+print(Rev(lst)) 
+
+
